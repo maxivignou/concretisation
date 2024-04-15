bc9d2374e34b54f5970a4dd6886b5cbceb7b10ae
maxivignou <166513268+maxivignou@users.noreply.github.com>
2024-04-15 16:59:09 +0200

    Update maStructure.cpp
    Copier-coller des prototypes des fonctions

--- maStructure.cpp
+++ maStructure.cpp
@@ -1,13 +1,21 @@
 #include "maStructure.h"
 
+void init_tableau(tableau & T, int taille); // Initialisation du tableau avec des valeurs nulles
 
+void recup_donnees(std::string nom_fichier, tableau & T, int & taille); // Création de la matrice carrée avec les données du fichier
 
+int somme_colonne_ligne(tableau & T, int taille, int no_somme); // Somme des valeurs dans la no_somme ligne et la no_somme colonne
 
+void creation_toutes_sommes(tableau & T, int taille, ligne & L_S); // Création de la liste contenant toutes les sommes
 
+int indice_min(ligne & L, int taille); // Récupération de l'indice minimal de la liste
 
+void modifications_donnees(tableau & T, int taille, ligne & L_S, ligne & M, int no_modification); // Modifications de toutes les données lorsqu'on appliquera un nouveau masque
 
+void application_masque(tableau & T, int taille, ligne & L_S, ligne & M); // Création masque total en modifiant les données
 
+int somme_tableau(tableau & T, int taille, ligne & M); // Somme de toutes les valeurs du tableau
 
+void renvoi_resultat(std::string nom_fichier, ligne & masque, int score); // Création du fichier qui contient le masque et le score
 
-
-
+void coeur_programme(std::string & fic_import, std::string & fic_export = "resultats.txt", tableau & T, ligne & M, int & taille); // Gestion du programme
