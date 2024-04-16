26498f80c3258f108ab165ea799aeba190064ecd
Bonjourme <131484603+Bonjourme@users.noreply.github.com>
2024-04-16 09:51:34 +0200

    Update monProgramme.cpp
    Remplissage du fichier monProgramme

--- monProgramme.cpp
+++ monProgramme.cpp
@@ -1,1 +1,12 @@
+#include "maStructure.h"
+#include "maStructure.cpp"
+
+int main() {
+  std::string nom_depart,nom_fin;
+  tableau T;
+  ligne M;
+  int taille;
+  coeur_programme(nom_depart,nom_fin,T,M,taille);
+  return 0;
+}
 
