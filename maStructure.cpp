e9eda12cf61be5ce1db145cde8708c02b1849339
Bonjourme <131484603+Bonjourme@users.noreply.github.com>
2024-04-16 09:23:19 +0200

    Update maStructure.cpp
    Ajout du script de la fonction somme_colonne_ligne

--- maStructure.cpp
+++ maStructure.cpp
@@ -25,7 +25,14 @@ void recup_donnees(std::string nom_fichier, tableau & T, int & taille) { // Cré
   }
 }
 
-int somme_colonne_ligne(tableau & T, int taille, int no_somme); // Somme des valeurs dans la no_somme ligne et la no_somme colonne
+int somme_colonne_ligne(tableau & T, int taille, int no_somme) { // Somme des valeurs dans la no_somme ligne et la no_somme colonne
+    int Somme = 0;
+    for (int i = 0; i < taille; i++) {
+        Somme = Somme + T[i][no_somme] + T[no_somme][i]; 
+    }
+    Somme = Somme - T[no_somme][no_somme];
+    return Somme;
+}
 
 void creation_toutes_sommes(tableau & T, int taille, ligne & L_S); // Création de la liste contenant toutes les sommes
 
