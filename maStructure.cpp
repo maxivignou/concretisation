f6d96b079e9811dcf592a4798eab8b40c5a17ef5
maxivignou <166513268+maxivignou@users.noreply.github.com>
2024-04-17 13:14:56 +0200

    Update maStructure.cpp
    Modification de la fonction coeur_programme pour le bon fonctionnement après la modification de la fonction application_masque

--- maStructure.cpp
+++ maStructure.cpp
@@ -129,8 +129,12 @@ void coeur_programme(std::string & fic_import, std::string & fic_export, tableau
     recup_donnees(fic_import,T,taille);
     ligne L_S;
     creation_toutes_sommes(T,taille,L_S);
-    application_masque(T,taille,L_S,M);
-    int S_T = somme_tableau(T,taille,M);
+    M = new int[taille];
+    for (int i = 0; i < taille; ++i) {
+        M[i] = 1;
+    }
+    int S_T;
+    application_masque(T,taille,L_S,M, S_T);
     renvoi_resultat(fic_export,M,S_T,taille);
     for (int i = 0; i < taille; i++) {
         delete[] T[i];
