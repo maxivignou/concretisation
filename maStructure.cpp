2f9b3938eceac0476fd55809d2c872fd8996aed8
maxivignou <166513268+maxivignou@users.noreply.github.com>
2024-04-17 14:29:04 +0200

    Update maStructure.cpp
    Optimisation : changement des codes des fonctions

--- maStructure.cpp
+++ maStructure.cpp
@@ -42,9 +42,9 @@ void creation_toutes_sommes(tableau & T, int taille, ligne & L_S) { // Création
 }
 
 int indice_min(ligne & L, int taille, ligne & M) { // Récupération de l'indice minimal de la liste
-    int i_min = 0;
-    for (int i = 1; i < taille; i++) {
-        if (L[i] < L[i_min] and M[i] != 2) {
+    int i_min = -1;
+    for (int i = 0; i < taille; i++) {
+        if ((i_min < 0 or L[i] < L[i_min]) and M[i] != 2) {
             i_min = i;
         }
     }
@@ -52,39 +52,43 @@ int indice_min(ligne & L, int taille, ligne & M) { // Récupération de l'indice
 }
 
 void modifications_donnees(tableau & T, int taille, ligne & L_S, ligne & M, int no_modification) { // Modifications de toutes les données lorsqu'on appliquera un nouveau masque
-    M[no_modification] = 0;
     for (int i = 0; i < taille; ++i) {
-        L_S[i] -= T[i][no_modification];
-        if (no_modification != i) {
-            L_S[i] -= T[no_modification][i];
-            L_S[no_modification] -= T[i][no_modification];
-            L_S[no_modification] -= T[no_modification][i];
+        if (M[i] != 0) {
+            L_S[i] -= T[i][no_modification];
+            if (no_modification != i) {
+                L_S[i] -= T[no_modification][i];
+                L_S[no_modification] -= T[i][no_modification];
+                L_S[no_modification] -= T[no_modification][i];
+            }
         }
     }
+    M[no_modification] = 0;
 }
 
 void application_masque(tableau & T, int taille, ligne & L_S, ligne & M, int & score) { // Création masque total en modifiant les données
     int mini = indice_min(L_S, taille, M);
-    if (L_S[mini] >= 0) {
+    if (mini < 0) {
+        score = -1;
+    }
+    else if (L_S[mini] >= 0) {
         score = somme_tableau(T, taille, M);
     } else {
         int score_1, score_2;
-        ligne M1, M2;
+        ligne M1, M2, L_S1, L_S2;
         M1 = new int[taille];
         M2 = new int[taille];
+        L_S1 = new int[taille];
+        L_S2 = new int[taille];
         for (int i = 0; i < taille; ++i) {
-            if (M[i] != 2) {
-                M1[i] = M[i];
-                M2[i] = M[i];
-            } else {
-                M1[i] = 1;
-                M2[i] = 1;
-            }
+            L_S1[i] = L_S[i];
+            L_S2[i] = L_S[i];
+            M1[i] = M[i];
+            M2[i] = M[i];
         }
         M2[mini] = 2;
-        modifications_donnees(T, taille, L_S, M1, mini);
-        application_masque(T, taille, L_S, M1, score_1);
-        application_masque(T, taille, L_S, M2, score_2);
+        modifications_donnees(T, taille, L_S1, M1, mini);
+        application_masque(T, taille, L_S1, M1, score_1);
+        application_masque(T, taille, L_S2, M2, score_2);
         if (score_1 > score_2) {
             M = M1;
             score = score_1;
