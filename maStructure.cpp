17a15e3c418a7df9b63bfdfa92c5a3f9a2dcfa60
maxivignou <166513268+maxivignou@users.noreply.github.com>
2024-04-18 09:09:17 +0200

    Update maStructure.cpp
    Suppression de la fonction coeur_programme déplacée dans le fichier execution_programme.cpp

--- maStructure.cpp
+++ maStructure.cpp
@@ -71,22 +71,3 @@ void application_masque(tableau & T, int taille, ligne & L_S, ligne & M, int & s
         }
     }
 }
-
-void coeur_programme(std::string & fic_import, std::string & fic_export, tableau & T, ligne & M, int & taille) { // Gestion du programme
-    recup_donnees(fic_import,T,taille);
-    ligne L_S;
-    creation_toutes_sommes(T,taille,L_S);
-    M = new int[taille];
-    for (int i = 0; i < taille; ++i) {
-        M[i] = 1;
-    }
-    int S_T;
-    application_masque(T,taille,L_S,M, S_T);
-    renvoi_resultat(fic_export,M,S_T,taille);
-    for (int i = 0; i < taille; i++) {
-        delete[] T[i];
-    }
-    delete[] T;
-    delete[] M;
-    delete[] L_S;
-}
