560d18ab0fe95ba18e6b58ef7ba7ffcdf8dc7794
maxivignou <166513268+maxivignou@users.noreply.github.com>
2024-04-23 13:09:09 +0200

    Update methode_aleatoire.cpp

--- methode_aleatoire.cpp
+++ methode_aleatoire.cpp
@@ -1,22 +1,22 @@
 #include "maStructure.h"
 
-void maximum_aleatoire(tableau & T, int taille, ligne & M_alea, int & somme_alea) {
+void maximum_aleatoire(tableau & T, int taille, ligne & meilleur_masque, int & meilleure_somme) {
     int quantite;
     if (taille < 1000) {
         quantite = 2500;
     } else {
         quantite = 0;
     }
-    ligne M_test = new int[taille];
-    somme_alea = 0;
+    ligne masque_test = new int[taille];
+    meilleure_somme = 0;
     for (int i = 0; i < quantite; ++i) {
         for (int j = 0; j < taille; ++j) {
-            M_test[j] = rand()%2;
+            masque_test[j] = rand()%2;
         }
         int somme_test = somme_tableau(T, taille, M_test);
-        if (somme_test > somme_alea) {
-            copie(M_alea, M_test, taille);
-            somme_alea = somme_test;
+        if (somme_test > meilleure_somme) {
+            copie(meilleur_masque, masque_test, taille);
+            meilleure_somme = somme_test;
         }
     }
 }
