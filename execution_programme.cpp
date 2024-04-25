4ced117d69ad5bd4587e1bee5e40baa6fbc109ae
Bonjourme <131484603+Bonjourme@users.noreply.github.com>
2024-04-25 10:59:46 +0200

    Update execution_programme.cpp
    Ajout de la variante diviser pour régner

--- execution_programme.cpp
+++ execution_programme.cpp
@@ -16,16 +16,18 @@ void coeur_programme(std::string & fic_import, std::string & fic_export) { // Ge
     int somme_etude, somme_aleatoire = -1;
     if (taille <= 25) {
         force_brute(T, taille, masque_etude, somme_etude);
-    } else if (taille <= 40) {
-        application_masque_reccur(T, taille, liste_sommes, masque_etude, somme_etude);
-        for (int i = 0; i < taille; ++i) {
-            if (masque_etude[i] == 2) masque_etude[i] = 1;
-        }
-        maximum_aleatoire(T, taille, masque_aleatoire, somme_aleatoire, heure_depart);
     } else {
-        application_masque(T, taille, liste_sommes, masque_etude);
-        somme_etude = somme_tableau(T, taille, masque_etude);
-        maximum_aleatoire(T, taille, masque_aleatoire, somme_aleatoire, heure_depart);
+        if (taille <= 40) {
+            application_masque_reccur(T, taille, liste_sommes, masque_etude, somme_etude);
+            for (int i = 0; i < taille; ++i) {
+                if (masque_etude[i] == 2) masque_etude[i] = 1;
+            }
+        } else {
+            application_masque(T, taille, liste_sommes, masque_etude);
+            somme_etude = somme_tableau(T, taille, masque_etude);
+        }
+        gestion_regroupement(T, taille, masque_aleatoire, somme_aleatoire);
+        masque_modifie_aleatoire(T, masque_aleatoire, taille, somme_aleatoire, heure_depart);
     }
     if (somme_etude > somme_aleatoire) {
         renvoi_resultat(fic_export,masque_etude,somme_etude,taille);
