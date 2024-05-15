082de300d75ed71a08aab627e1d933bffb71f809
maxivignou <166513268+maxivignou@users.noreply.github.com>
2024-05-15 17:25:21 +0200

    Update methode_brute.cpp
    Mise en forme des fichiers

--- methode_brute.cpp
+++ methode_brute.cpp
@@ -1,34 +1,73 @@
-#include "maStructure.h"
+#include "prototypes.h"
 
-void ajoute_un(ligne & masque,int taille) {
-    int i = 0;
-    while ((i < taille) and (masque[i] == 1)) {
-        masque[i] = 0;
-        i += 1;
+void masque_suivant(ligne & masque, int taille) {
+    /*
+     Objectif :
+        On cherche un nouveau masque à tester. En faisant comme une addition en binaire, on obtient le masque suivant.
+     Paramètres :
+        masque : ligne -> Ce paramètre correspond au masque qui vient d'être testé, et qui contiendra le masque suivant.
+        taille : int -> Ce paramètre indique la taille du masque.
+     Retour :
+        Pas de retour, mais le paramètre masque contient le masque obtenu par la fonction.
+     */
+    
+    int parcours = 0;
+    
+    while ( (parcours < taille) and (masque[parcours] == 1) ) { // Il s'agit de gérer les éventuelles retenues
+        masque[parcours] = 0;
+        parcours += 1;
     }
-    if (i != taille) masque[i] = 1;
+    
+    if (parcours != taille) {
+        masque[parcours] = 1;
+    }
 }
 
-void copie(ligne & masque_recevant, ligne & masque_envoyant, int taille) {
-    for (int i = 0; i < taille; ++i) {
-        masque_recevant[i] = masque_envoyant[i];
+void copie_masque(ligne & masque_recevant, ligne & masque_envoyant, int taille) {
+    /*
+     Objectif :
+        On cherche à copier un masque. On copie alors élément par élément masque_envoyant dans masque_recevant.
+     Paramètres :
+        masque_recevant : ligne -> Ce paramètre correspond au masque qui recevra la copie.
+        masque_envoyant : ligne -> Ce paramètre correspond au masque que l'on doit copier.
+        taille : int -> Ce paramètre indique la taille du masque.
+     Retour :
+        Pas de retour, mais le paramètre masque_recevant contient le masque obtenu par la fonction.
+     */
+    
+    for (int parcours = 0; parcours < taille; ++parcours) {
+        masque_recevant[parcours] = masque_envoyant[parcours];
     }
 }
 
-void force_brute(tableau & T, int taille, ligne & meilleur_masque, int & meilleure_somme) {
-    meilleur_masque = new int[taille];
+void recherche_masque_brute(tableau & T, int taille, ligne & masque, int & somme) {
+    /*
+     Objectif :
+        On veut obtenir le masque ayant la plus grande somme possible.
+     Paramètres :
+        T : tableau -> Ce paramètre est le tableau sur lequel on doit appliquer le masque.
+        taille : int -> Ce paramètre indique la taille du tableau et par conséquent du masque.
+        masque : ligne -> Ce paramètre correspond au meilleur masque possible.
+        somme : int -> Ce paramètre correspond à la meilleure somme possible.
+     Retour :
+        Pas de retour, mais les paramètres masque et somme contiennent les résultats attendus.
+     */
+    
+    masque = new int[taille];
     ligne masque_test = new int[taille];
-    for (int i = 0; i < taille; ++i) {
-        meilleur_masque[i] = 0;
-        masque_test[i] = 0;
+    somme = 0;
+    
+    for (int parcours = 0; parcours < taille; ++parcours) { // Il s'agit d'initialiser les masques
+        masque[parcours] = 0;
+        masque_test[parcours] = 0;
     }
-    meilleure_somme = 0;
-    for (int i = 0; i < std::pow(2, taille); ++i) {
-        ajoute_un(masque_test, taille);
+    
+    for (int parcours = 0; parcours < std::pow(2, taille); ++parcours) {
+        masque_suivant(masque_test, taille);
         int somme_test = somme_tableau(T, taille, masque_test);
-        if (somme_test > meilleure_somme) {
-            meilleure_somme = somme_test;
-            copie(meilleur_masque, masque_test, taille);
+        if (somme_test > somme) {
+            somme = somme_test;
+            copie_masque(masque, masque_test, taille);
         }
     }
 }
